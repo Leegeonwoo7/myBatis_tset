68385a0af8ef069f5ddde5ec09c6b48bad7c7ff9
Leegeonwoo7 <workgeon07@naver.com>
2024-02-15 02:47:29 +0900

    dao

--- src/main/java/user/dao/UserDAO.java
+++ src/main/java/user/dao/UserDAO.java
@@ -2,7 +2,9 @@ package user.dao;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -24,7 +26,6 @@ public class UserDAO {
             String resource = "mybatis-config.xml";
             InputStream inputStream = Resources.getResourceAsStream(resource);
             sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -38,21 +39,37 @@ public class UserDAO {
     }
     public List<UserDTO> getUserList() {
         SqlSession sqlSession = sqlSessionFactory.openSession();//생성
-        List<UserDTO>list=sqlSession.selectList("userSQL.getUserList");
+        List<UserDTO> list=sqlSession.selectList("userSQL.getUserList");
         sqlSession.close();
         return list;
     }
 
     public UserDTO findId(String id) {
         SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserDTO userDTO = sqlSession.selectOne("userSQL.findId", id);
         sqlSession.close();
-        return sqlSession.selectOne("userSQL.findId", id);
-
+        return userDTO;
     }
 
-
-    public boolean update(String name){
-
+    public void updateUser(UserDTO userDTO) {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        sqlSession.update("userSQL.update", userDTO);
+        sqlSession.commit();
+        sqlSession.close();
     }
 
+    public void deleteUser(UserDTO userDTO) {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        sqlSession.delete("userSQL.delete", userDTO);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    public List<UserDTO> search(Map<String, String> map){
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        List<UserDTO> list= sqlSession.selectList("userSQL.search", map);
+        sqlSession.close();
+        return list;
+    }
 }
+
