dcc65de761e593c78e7df0c0be0df296cd3e2675
Leegeonwoo7 <workgeon07@naver.com>
2024-02-15 02:47:14 +0900

    update

--- src/main/java/user/service/UserUpdateService.java
+++ src/main/java/user/service/UserUpdateService.java
@@ -1,5 +1,6 @@
 package user.service;
 
+import user.bean.UserDTO;
 import user.dao.UserDAO;
 
 import java.util.Scanner;
@@ -9,12 +10,33 @@ public class UserUpdateService  implements UserService {
     @Override
     public void execute() {
         Scanner scan = new Scanner(System.in);
+        UserDAO userDAO = UserDAO.getInstance();
 
         System.out.println("찾고자 하는 아이디 입력: ");
-        String name = scan.next();
+        String id = scan.nextLine();
+        UserDTO userDTO = userDAO.findId(id);
+        if(userDTO == null){
+            System.out.println("찾고자 하는 아이디가 없습니다.");
+            return;
+        } else {
+            System.out.print("수정할 이름 입력 : ");
+            String newName = scan.nextLine();
+            userDTO.setName(newName);
 
-        UserDAO userDAO = UserDAO.getInstance();
-        boolean isVal = userDAO.update(name);
+            System.out.print("수정할 비밀번호 입력 : ");
+            String newPwd = scan.nextLine();
+            userDTO.setPwd(newPwd);
+
+            userDAO.updateUser(userDTO);
+
+            System.out.println("회원정보 수정완료");
+            System.out.println(userDTO.getName()
+                    + "\t" +userDTO.getId()
+                    + "\t" + userDTO.getPwd());
+        }
+
+
+
 
 //        찾고자 하는 아이디 입력 : angel
 //        찾고자 하는 아이디 없습니다.
@@ -29,5 +51,4 @@ public class UserUpdateService  implements UserService {
 //        회원정보 수정완료
 
     }
-
 }
