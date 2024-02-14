5ebdc98f066c5bcad1b1e2c4ff8dbf71d0a8bc6c
Leegeonwoo7 <workgeon07@naver.com>
2024-02-15 02:45:50 +0900

    delete

--- src/main/java/user/service/UserDeleteService.java
+++ src/main/java/user/service/UserDeleteService.java
@@ -1,15 +1,29 @@
 package user.service;
 
+import user.bean.UserDTO;
+import user.dao.UserDAO;
+
+import java.util.Scanner;
+
 public class UserDeleteService implements UserService {
 
     @Override
     public void execute() {
-//        찾고자 하는 아이디 입력 : angel
-//        찾고자 하는 아이디 없습니다.
-//
-//        또는
-//        찾고자 하는 아이디 입력 : hong
+            Scanner scan = new Scanner(System.in);
+            UserDAO userDAO = UserDAO.getInstance();
+
+            System.out.println("삭제 하려는 아이디 입력 : ");
+            String id = scan.next();
+
+            UserDTO userDTO = userDAO.findId(id);
+            if (userDTO == null) {
+                System.out.println("찾고자 하는 아이디 없습니다.");
+                return;
+            }
+            System.out.println(userDTO.getName() + "\t" + userDTO.getId() + "\t" + userDTO.getPwd());
+            userDAO.deleteUser(userDTO);
+            System.out.println("삭제되었습니다.");
+
+        }
 
     }
-
-}
